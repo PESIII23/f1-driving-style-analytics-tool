75dcae3346c0e98b3a0f4343875480292b541977
Phillip Smith <phillipsmith@Mac.attlocal.net>
2025-09-10 00:32:04 -0500

    added markdown to single race

--- src/data/f1data.py
+++ src/data/f1data.py
@@ -5,16 +5,19 @@ class F1Session:
     
     def __init__(self, year: int, gp: str, session: str):
         self.session = fastf1.get_session(year, gp, session)
-        self.session.load()
+        self.session.load(telemetry=True)
     
     def get_laps(self, driver: str=None):
-        return self.session.laps.pick_driver(driver) if driver else self.session.laps
-    
+        return self.session.laps.pick_drivers(driver).sess if driver else self.session.laps
+
     def get_fastest_lap(self, driver: str):
-        return self.session.laps.pick_driver(driver).pick_fastest()
-    
+        return self.session.laps.pick_drivers(driver).pick_fastest()
+
     def get_telemetry(self, lap):
         return lap.get_telemetry()
+    
+    def get_weather_data(self):
+        return self.session.weather_data
     
     # Delegate directly to session if needed
     def __getattr__(self, name):
