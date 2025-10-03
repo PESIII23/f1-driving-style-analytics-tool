1ddea2fcbaa0230b4896d96e164d622ef305d5da
Phillip Smith <phillipsmith@Mac.attlocal.net>
2025-10-03 12:46:19 -0500

    added jerk and get df function to each driver

--- src/preprocessing/feature_engineering.py
+++ src/preprocessing/feature_engineering.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class TelemetryFeatures:
     def __init__(self, df):
-        self.df = df
+        self.df = df.copy()
 
     def acceleration(self, speed='Speed (m/s)', sector_time='SectorTime (s)'):
         """
@@ -14,7 +14,7 @@ class TelemetryFeatures:
         self.df[speed] = pd.to_numeric(self.df[speed], errors='coerce')
         self.df[sector_time] = pd.to_timedelta(self.df[sector_time], errors='coerce')
         self.df['Acceleration (m/s²)'] = (self.df[speed].shift(-1) - self.df[speed])/(self.df[sector_time].shift(-1) - self.df[sector_time]).dt.total_seconds()
-        self.df.loc[self.df.index[-1], 'Acceleration (m/s²)'] = self.df.loc[self.df.index[-3:].mean(), 'Acceleration (m/s²)']
+        self.df.loc[self.df.index[-1], 'Acceleration (m/s²)'] = self.df['Acceleration (m/s²)'].iloc[-3:].mean()
 
         cols = self.df.columns.tolist()
         cols.remove('Acceleration (m/s²)')
@@ -22,10 +22,29 @@ class TelemetryFeatures:
         cols.insert(speed_index + 1, 'Acceleration (m/s²)')
         self.df = self.df[cols]
 
-        return self.df
+        return self
 
-    # def jerk(self):
-    #     self.df['Jerk (m/s³)'] = ...
-    #     return self.df
+    def jerk(self, accel='Acceleration (m/s²)', sector_time='SectorTime (s)'):
+        """
+        Calculates jerk as ΔAcceleration / ΔSectorTime, inserts the resulting column
+        immediately after the acceleration column, and sets the final row to the average of 
+        the previous three jerk values to produce a realistic ending.
+        """
+
+        self.df[accel] = pd.to_numeric(self.df[accel], errors='coerce')
+        self.df[sector_time] = pd.to_timedelta(self.df[sector_time], errors='coerce')
+        self.df['Jerk (m/s³)'] = (self.df[accel].shift(-1) - self.df[accel])/(self.df[sector_time].shift(-1) - self.df[sector_time]).dt.total_seconds()
+        self.df.loc[self.df.index[-1], 'Jerk (m/s³)'] = self.df['Jerk (m/s³)'].iloc[-3:].mean()
+
+        cols = self.df.columns.tolist()
+        cols.remove('Jerk (m/s³)')
+        accel_index = cols.index(accel)
+        cols.insert(accel_index + 1, 'Jerk (m/s³)')
+        self.df = self.df[cols]
+
+        return self
+    
+    def get_features_df(self):
+        return self.df
     
     # def find_steering_wheel_angle(df, driver: str):
